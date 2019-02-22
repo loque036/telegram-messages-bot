f8afa39c5531ff8a5f6a900ea761e18e84cb0e59
loque036 <30961953+loque036@users.noreply.github.com>
2019-02-23 00:46:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,13 @@ bot = telepot.Bot("Bot_API_Key",)
 # Number your counter starts with
 counter = 0
 while True:
-    counter = counter+1
-    bot.sendMessage("Your_ID", "{c}".format(c=counter))
-    time.sleep(1.5)
-    sys.stdout.write("\r" + str(counter))
+    try:
+        counter = counter+1
+        bot.sendMessage("Your_ID", "{c}".format(c=counter))
+        sys.stdout.write("\r" +str(counter))
+    except:
+        raise
+        sys.stdout.write("\r" +str(counter))
+    finally:
+        time.sleep(1.5)
+        counter = counter
